7e9f9133ce9e951796134b54e5f39649141c7b42
elaynelemos <elayne.l.lemos@gmail.com>
2022-11-13 18:22:21 -0300

    feat: refact request

--- sentences_correction.py
+++ sentences_correction.py
@@ -5,23 +5,22 @@ import datetime
 import concurrent.futures
 import sys
 
-HOST = 'https://local-scenario'
-API_PATH = '/correct'
-ENDPOINT = HOST + API_PATH
+API_URL = f'{sys.argv[1]}'
+REQUEST_ROUTE = '/correct'
+ENDPOINT = API_URL + REQUEST_ROUTE
 MAX_THREADS = 1024
 
 def send_api_request(sentence):
     r = requests.get(ENDPOINT, data={'sentence': sentence})
-    print ('Received: ', datetime.datetime.now(), r.content)
+    print('Received:', datetime.datetime.now(), r.content)
 
 
 if __name__ == '__main__':
-    start_time = datetime.datetime.now()
-    print ('Starting:', start_time)
+    print ('Starting:', datetime.datetime.now())
 
     for i in range(11):
         n_sentences = 2**i
-        input_path = f'./data/conll/iter_{sys.argv[1]}/official-2014.{n_sentences}-sentences.combined.src'
+        input_path = f'./data/conll/iter_{sys.argv[2]}/official-2014.{n_sentences}-sentences.combined.src'
 
         with open(input_path, 'r', encoding='utf-8') as f:
             sentence_list = f.read().splitlines()
@@ -30,7 +29,7 @@ if __name__ == '__main__':
         print('Running for:', len(sentence_list),'(sentences)\n')
         with concurrent.futures.ThreadPoolExecutor(MAX_THREADS) as executor:
             futures = [ executor.submit(send_api_request(s)) for s in sentence_list ]
-        
+
         time.sleep(5)
 
     print('\nEnd time:', datetime.datetime.now())
