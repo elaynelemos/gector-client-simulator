6eab0c0393845e9c23374e51e47b2535788e15d7
elaynelemos <elayne.l.lemos@gmail.com>
2022-11-20 18:59:26 -0300

    add: uuids

--- sentences_correction.py
+++ sentences_correction.py
@@ -5,6 +5,7 @@ from datetime import datetime
 import concurrent.futures
 import sys
 import logging
+from uuid import uuid4
 
 API_URL = f'{sys.argv[1]}'
 REQUEST_ROUTE = '/correct'
@@ -21,28 +22,30 @@ logging.basicConfig(
 
 log = logging.getLogger()
 
-def send_api_request(sentence):
+def send_api_request(sentence, id):
     global ENDPOINT
     global HEADERS
 
-    log.info(f'[SENT] {sentence}')
+    log.info(f'[SENT][{id}] {sentence}')
     r = requests.post(ENDPOINT, headers=HEADERS, data='{"sentence": '+ f'"{sentence}"' + '}')
-    log.info(f'[RECEIVED] {r.text}')
+    log.info(f'[RECEIVED][{id}] {r.text}')
 
 if __name__ == '__main__':
     log.info('============== Starting GECToR load simulation! ==============')
 
     for i in range(11):
         n_sentences = 2**i
-        input_path = f'./data/conll/iter_{sys.argv[2]}/official-2014.{n_sentences}-sentences.combined.src'
+        uuids = [ str(uuid4()).replace("-", "") for _ in range(n_sentences) ]
 
+        input_path = f'./data/conll/iter_{sys.argv[2]}/official-2014.{n_sentences}-sentences.combined.src'
         with open(input_path, 'r', encoding='utf-8') as f:
             sentence_list = f.read().splitlines()
 
         log.info('\n========================= New load ==========================')
         log.info(f'Running for: {len(sentence_list)} sentences\n')
+
         with concurrent.futures.ThreadPoolExecutor(MAX_THREADS) as executor:
-            executor.map(send_api_request, sentence_list)
+            executor.map(send_api_request, sentence_list, uuids)
 
         time.sleep(5)
 
