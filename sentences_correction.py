b18ce540ad1f0848801d3c128435fd3d80841576
elaynelemos <elayne.l.lemos@gmail.com>
2022-12-07 08:46:44 -0300

    feat: use dataframe to store data

--- sentences_correction.py
+++ sentences_correction.py
@@ -6,12 +6,14 @@ import concurrent.futures
 import sys
 import logging
 from uuid import uuid4
+import pandas as pd
 
 API_URL = f'{sys.argv[1]}'
 REQUEST_ROUTE = '/correct'
 ENDPOINT = API_URL + REQUEST_ROUTE
 MAX_THREADS = 1024
 HEADERS = {'Content-Type': 'application/json'}
+df = pd.DataFrame(columns=['sent_time','sent', 'received_time', 'received'])
 
 logging.basicConfig(
     filename=f'client-simulation-iter_{sys.argv[2]}-{int(datetime.now().timestamp())}.log',
@@ -22,21 +24,24 @@ logging.basicConfig(
 
 log = logging.getLogger()
 
-def send_api_request(sentence, id):
+def send_api_request(sentence):
     global ENDPOINT
     global HEADERS
+    global df
 
-    log.info(f'[SENT][{id}] {sentence}')
+    sent_time = datetime.now().timestamp()
     r = requests.post(ENDPOINT, headers=HEADERS, data='{"sentence": '+ f'"{sentence}"' + '}')
-    log.info(f'[RECEIVED][{id}] {r.text}')
+
+    received_time = datetime.now().timestamp()
+    df = df.append(dict(zip(df.columns,[sent_time, sentence, received_time, r.text])))
 
 if __name__ == '__main__':
     log.info('============== Starting GECToR load simulation! ==============')
     init_time = datetime.now()
 
     for i in range(11):
+        df.drop(df.index, inplace=True)
         n_sentences = 2**i
-        uuids = [ str(uuid4()).replace("-", "") for _ in range(n_sentences) ]
 
         input_path = f'./data/conll/iter_{sys.argv[2]}/official-2014.{n_sentences}-sentences.combined.src'
         with open(input_path, 'r', encoding='utf-8') as f:
@@ -46,7 +51,8 @@ if __name__ == '__main__':
         log.info(f'Running for: {len(sentence_list)} sentences\n')
 
         with concurrent.futures.ThreadPoolExecutor(MAX_THREADS) as executor:
-            executor.map(send_api_request, sentence_list, uuids)
+            executor.map(send_api_request, sentence_list)
+
 
         time.sleep(5)
 
