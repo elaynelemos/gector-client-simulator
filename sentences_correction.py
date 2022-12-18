b58b64a8d8fd5797c233f48b76ced6fab807d061
elaynelemos <elayne.l.lemos@gmail.com>
2022-12-18 20:14:09 -0300

    fix: dataframe parallel operation

--- sentences_correction.py
+++ sentences_correction.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python3
 import time
 import requests
 from datetime import datetime
@@ -13,7 +12,7 @@ REQUEST_ROUTE = '/correct'
 ENDPOINT = API_URL + REQUEST_ROUTE
 MAX_THREADS = 1024
 HEADERS = {'Content-Type': 'application/json'}
-df = pd.DataFrame(columns=['sent', 'received', 'received_time', 'latency', 'status_code'])
+DF_COLUMNS = ['sent', 'received', 'received_time', 'latency', 'status_code']
 
 logging.basicConfig(
     filename=f'client-simulation-iter_{sys.argv[2]}-{int(datetime.now().timestamp())}.log',
@@ -27,34 +26,38 @@ log = logging.getLogger()
 def send_api_request(sentence):
     global ENDPOINT
     global HEADERS
-    global df
+    global DF_COLUMNS
 
     r = requests.post(ENDPOINT, headers=HEADERS, data='{"sentence": '+ f'"{sentence}"' + '}')
-
-    df = df.append(dict(zip(df.columns,[sentence, r.text, datetime.now().timestamp(), r.elapsed, r.status_code])))
+    return dict(zip(DF_COLUMNS, [sentence, r.text, int(datetime.now().timestamp()), r.elapsed.total_seconds(), r.status_code]))
 
 if __name__ == '__main__':
-    log.info('============== Starting GECToR load simulation! ==============')
+    log.info('======= Starting GECToR load simulation! =======')
     init_time = datetime.now()
 
     for i in range(11):
-        df.drop(df.index, inplace=True)
+        results = []
         n_sentences = 2**i
 
         input_path = f'./data/conll/iter_{sys.argv[2]}/official-2014.{n_sentences}-sentences.combined.src'
         with open(input_path, 'r', encoding='utf-8') as f:
             sentence_list = f.read().splitlines()
 
-        log.info('\n========================= New load ==========================')
         log.info(f'Running for: {len(sentence_list)} sentences\n')
-
         with concurrent.futures.ThreadPoolExecutor(MAX_THREADS) as executor:
-            executor.map(send_api_request, sentence_list)
+            for result in executor.map(send_api_request, sentence_list):
+                try:
+                    results.append(result)
+                except Exception as ex:
+                    print(str(ex))
+                    pass
 
-        df.to_csv(f'client-simulation-iter_{sys.argv[2]}-{n_sentences}.csv')
+        df = pd.DataFrame(results, columns=DF_COLUMNS)
+        df.to_csv(f'client-simulation-iter_{sys.argv[2]}-{n_sentences}.csv', index=False)
+
         time.sleep(5)
 
     end_time = datetime.now()
 
-    log.info('=============== Ending GECToR load simulation! ===============')
+    log.info('======= Ending GECToR load simulation! =======')
     log.info(f'Simulation duration: {end_time - init_time}')
