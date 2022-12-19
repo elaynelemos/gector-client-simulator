50fb644444c0b19decb91ef9a2d6d27afc88834d
elaynelemos <elayne.l.lemos@gmail.com>
2022-12-19 09:16:14 -0300

    rm: temporary file

--- get_simulation_timestamps.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-dates=$(grep -E '(INFO - Running for)|(Ending GECToR)' -B 4 $1 | grep RECEIVED | awk -F ' - ' '{printf "%s\n", $1}')
-while read line; do date --date="$line" +"%s"; done <<< "$dates"
